7b42e81ceff6ce3592af7efc540643956ad28ac4
Rainnny7 <braydonrainnny@gmail.com>
2025-04-23 04:06:08 -0400

    When loading the site, start from the top

--- src/provider/app-providers.tsx
+++ src/provider/app-providers.tsx
@@ -2,19 +2,26 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ViewTransitions } from "next-view-transitions";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { TooltipProvider } from "~/components/ui/tooltip";
 import SidebarProvider from "~/provider/sidebar-provider";
 
 const queryClient = new QueryClient();
 
-const AppProviders = ({ children }: { children: ReactNode }) => (
-    <QueryClientProvider client={queryClient}>
-        <TooltipProvider delayDuration={100}>
-            <ViewTransitions>
-                <SidebarProvider>{children}</SidebarProvider>
-            </ViewTransitions>
-        </TooltipProvider>
-    </QueryClientProvider>
-);
+const AppProviders = ({ children }: { children: ReactNode }) => {
+    // When loading the site, start from the top
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <TooltipProvider delayDuration={100}>
+                <ViewTransitions>
+                    <SidebarProvider>{children}</SidebarProvider>
+                </ViewTransitions>
+            </TooltipProvider>
+        </QueryClientProvider>
+    );
+};
 export default AppProviders;
