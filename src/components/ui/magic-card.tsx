a7f438c47a77152d0ded6ec6dd26fc24a211ca3c
Rainnny7 <braydonrainnny@gmail.com>
2025-04-23 11:38:31 -0400

    fix the project content not being proper height

--- src/components/ui/magic-card.tsx
+++ src/components/ui/magic-card.tsx
@@ -102,7 +102,7 @@ export function MagicCard({
                     opacity: gradientOpacity,
                 }}
             />
-            <div className="relative">{children}</div>
+            <div className="h-full relative">{children}</div>
         </div>
     );
 }
