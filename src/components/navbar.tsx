0aa3dc725d652b9c66711046c67a47df74744f25
Rainnny7 <braydonrainnny@gmail.com>
2025-04-24 16:10:15 -0400

    hide the kitty toggle on mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -225,7 +225,7 @@ const KittyToggle = (): ReactElement => {
     return (
         <Button
             className={cn(
-                "size-8 rounded-xl hover:bg-zinc-900/55 transition-all transform-gpu",
+                "hidden md:block size-8 rounded-xl hover:bg-zinc-900/55 transition-all transform-gpu",
                 showKitty && "bg-zinc-900/55 text-primary/85 hover:text-primary"
             )}
             variant="ghost"
